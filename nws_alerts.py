2eaa43dfc496071f314ccb10f18cdc2a519d56a5
Zeb Palmer <zeb@zebpalmer.com>
2011-12-04 21:18:52 -0700

    readme, todo and command line changes

--- nws_alerts.py
+++ nws_alerts.py
@@ -2,10 +2,10 @@
 
 '''
     Project home: git.zebpalmer.com/nws-alerts     
+    Original Author: Zeb Palmer   (www.zebpalmer.com)
     For more info, please see the README.txt
 
-    This file is part of pyForecaster, (to be released shortly)
-    this program is free software you can redistribute it and
+    This program is free software you can redistribute it and
     or modify it under the terms of the GNU General Public License 
     as published by the Free Software Foundation, either version 
     3 of the License, or (at your option) any later version.
@@ -17,6 +17,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 '''
 
@@ -311,9 +312,5 @@ if __name__ == "__main__":
         if type == 'state':
             cap = CapAlerts(state=sys.argv[2])
             cap.print_summary(cap.summary(cap.alerts_by_state(sys.argv[2])))
-
-    #import cProfile
-    #import pstats
-    #cProfile.run("cap=CapAlerts()", 'Cap')
-    #p = pstats.Stats('Cap')
-    #p.sort_stats('time').print_stats(20)
+    else:
+        print "No arguments supplied" 
