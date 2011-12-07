ac751a277d285a9d4c9c84ecd2fe3f615137a5dd
Zeb Palmer <zeb@zebpalmer.com>
2011-12-06 21:33:02 -0700

    moving to module/pkg dir structure, cache move, var changes

--- nagios_plugin.py
+++ nagios_plugin.py
@@ -3,26 +3,26 @@
 
 
 ### proof of concept only at the moment, would like to add user options to set nagios status levels based on alert type
-### need to add that, as well as the location below to a settings file. 
+### need to add that, as well as the location below to a settings file.
 
 
 
 from sys import exit
-from nws_alerts import CapAlerts
+from nws_alerts import nws_alerts
 
 ## Enter your state abbreviation as well as your county (same code not supported yet)
 
-state = 'ID' 
+state = 'ID'
 county = 'Canyon'
 samecode = ''
 
 def location(state, county='', samecode=''):
     # not the most elegant way to do this, but this is mostly a place
-    # holder as we will support multiple locations, and other location 
+    # holder as we will support multiple locations, and other location
     # features soon
     local = {}
     if not county or samecode:
-        print "Must set County or SAME code"
+        print("Must set County or SAME code")
         exit(3)
     if len(samecode) == 6:
         local['same'] = samecode
@@ -33,18 +33,18 @@ def location(state, county='', samecode=''):
         local['state'] = state
         local['type'] = 'county'
     return local
-        
+
 
 
 def check_alerts(location):
     if location['type'] == 'county':
-        cap = CapAlerts(location['state'])
+        cap = nws_alerts.CapAlerts(location['state'])
         alerts = cap.alerts_by_county_state(location['county'], location['state'])
         if len(alerts) == 0:
-            print "No active alerts"
+            print("No active alerts")
             exit(0)
         elif len(alerts) == 1:
-            print cap.alerts.alerts_type()
+            print(cap.alerts.alerts_type())
         else:
             #print "{0} Alerts".format(len(alerts))
             types = []
@@ -53,13 +53,13 @@ def check_alerts(location):
                 if alert_type not in types:
                     types.append(alert_type)
             for alert_type in types:
-                print alert_type + ',',
+                print(alert_type + ',', end=' ')
         exit(1)
-        
-            
-            
+
+
+
     elif location['type'] == 'same':
-        print "Not yet supported"
+        print("Not yet supported")
         exit(3)
 
 
