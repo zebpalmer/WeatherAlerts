1760d7e9fbc0b46c983b28cd155c23c687346ae6
Zeb Palmer <zeb@zebpalmer.com>
2011-12-18 19:30:36 -0700

    adding test script to install

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
     package_dir={
         'weatheralerts': base_dir + "/weatheralerts"},
     scripts=[ base_dir + "/NagiosWeatherAlerts.py",
-              base_dir + "/MonitorAlertsByCounty.py"],
+              base_dir + "/MonitorAlertsByCounty.py",
+              base_dre + "/test_WeatherAlerts_scripts.py"]
     url='http://github.com/zebpalmer/WeatherAlerts',
     license='GPLv3',
     description='Parse the National Weather Service Emergency Alerts Feed, do useful stuff with it',
