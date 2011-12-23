e4322dba7b05880c5a1b7168fb70880272fda325
Zeb Palmer <zeb@zebpalmer.com>
2011-12-23 10:06:57 -0700

    reformatted file header in rst

--- python3/weatheralerts/nws.py
+++ python3/weatheralerts/nws.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 
 '''
-    Project home: github.com/zebpalmer/WeatherAlerts
-    Original Author: Zeb Palmer   (www.zebpalmer.com)
-    For more info, please see the README.rst
+WeatherAlerts.nws
+*******************
 
-    This program is free software you can redistribute it and
-    or modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation, either version
-    3 of the License, or (at your option) any later version.
+**Project Home:**
+  http://github.com/zebpalmer/WeatherAlerts
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+**Original Author:**
+  Zeb Palmer http://www.zebpalmer.com
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**Documentation:**
+  http://weatheralerts.readthedocs.org
+
+
+**License:**
+  GPLv3 - full text included in LICENSE.txt
+
+
+**License Notice:**
+  This program is free software you can redistribute it and or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+  any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.  You should have received a copy of the GNU General Public
+  License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 '''
+
 
 
 import os
