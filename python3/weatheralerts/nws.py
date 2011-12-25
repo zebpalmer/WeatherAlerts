cc4a7c85d2f4780f594f6a0808877bbaa622d1f7
Zeb Palmer <zeb@zebpalmer.com>
2011-12-24 18:09:14 -0700

    added method to create alert lookup table

--- python3/weatheralerts/nws.py
+++ python3/weatheralerts/nws.py
@@ -193,7 +193,7 @@ class SameCodes(object):
             if file_ts > maxage:
                 try:
                     cache = open(cache_file, 'rb')
-                    self.samecodes = pickle.load(cache)
+                    self._samecodes = pickle.load(cache)
                     cache.close()
                     #print "Loaded SAME codes from Cache"
                     return True
@@ -228,7 +228,9 @@ class CapAlertsFeed(object):
         self.samecodes = self.geo.samecodes
         self._cachetime = 3
         self._alerts_ts = datetime.now()
+        self.lookuptable = {}
         self._load_alerts()
+        print(self.lookuptable)
 
 
     @property
@@ -311,7 +313,7 @@ class CapAlertsFeed(object):
         return xml
 
     def _parse_cap(self, xmlstr):
-        '''parse the feed contents'''
+        '''parse and cache the feed contents'''
         main_dom = minidom.parseString(xmlstr)
 
         xml_entries = main_dom.getElementsByTagName('entry')
@@ -342,6 +344,7 @@ class CapAlertsFeed(object):
                 except IndexError:
                     return {}
             entry['type'] = pat.match(entry['title']).group(1)
+            
             locations = []
             for geo in entry['geocodes']:
                 try:
@@ -351,6 +354,11 @@ class CapAlertsFeed(object):
                                  'local': geo,
                                  'state': 'unknown'}
                 locations.append(location)
+
+                # We're going to create a table to reference alerts by
+                # TODO: make all alert lookups use the resulting data 
+                self._create_lookuptable(geo, entry_num, entry['type'])
+
             target_areas = []
             areas = str(entry['cap:areaDesc']).split(';')
             for area in areas:
@@ -359,14 +367,22 @@ class CapAlertsFeed(object):
             entry['target_areas'] = target_areas
             alerts[entry_num] = entry
             del entry
+        # cache alerts data
         cache = open(self._alert_cache_file, 'wb')
         pickle.dump(alerts, cache)
         cache.close()
         self._alerts_ts = datetime.now()
         return alerts
 
-
-
+    def _create_lookuptable(self, samecode, alert_num, alert_type):
+        alert_tuple = (alert_num, alert_type)
+        if samecode in self.lookuptable:
+            self.lookuptable[samecode].append(alert_tuple)
+        else:
+            _alertids = []
+            _alertids.append(alert_tuple)
+            self.lookuptable[samecode] = _alertids
+    
 
 class FormatAlerts(object):
     def __init__(self):
@@ -555,17 +571,17 @@ class Alerts(object):
                     location_alerts.append(alert_data[alert])
         return location_alerts
 
-    @property
-    def active_locations(self, alert_data):
-        '''returns list of all active locations'''
-        warned_areas = {}
-        for alert in alert_data.keys():
-            for location in alert_data[alert]['locations']:
-                if location['code'] not in list(warned_areas.keys()):
-                    warned_areas[location['code']] = [alert]
-                else:
-                    warned_areas[location['code']].append(alert)
-        return warned_areas
+    #@property
+    #def active_locations(self, alert_data):
+        #'''returns list of all active locations'''
+        #warned_areas = {}
+        #for alert in alert_data.keys():
+            #for location in alert_data[alert]['locations']:
+                #if location['code'] not in list(warned_areas.keys()):
+                    #warned_areas[location['code']] = [alert]
+                #else:
+                    #warned_areas[location['code']].append(alert)
+        #return warned_areas
 
 
 def alert_type(alert):
@@ -583,11 +599,5 @@ def alert_type(alert):
 
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
-    pass
+    nws_alerts = Alerts(state='ID')
